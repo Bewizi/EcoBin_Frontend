7064a669701e8496ad4ff519314a4909a33a9676
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-11-14 21:44:05 +0100

    feat:allowed access to my phone locally for testing the backend

--- lib/core/data/config/api_config.dart
+++ lib/core/data/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://127.0.0.1:8000/api/';
+  static const String baseUrl = 'http://192.168.0.2:8000/api/';
 
   static const String loginEndpoint = 'login';
   static const String registerEndpoint = 'register';
