7759296fdf7835bf0c7d6b9ddc7557ea8dd45199
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-11-24 20:37:48 +0100

    fix: Update base URL in ApiConfig to use 0.0.0.0 for local development

--- lib/core/data/config/api_config.dart
+++ lib/core/data/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://127.0.0.1:8000/api/';
+  static const String baseUrl = 'http://0.0.0.0:8000/api/';
 
   static const String loginEndpoint = 'login';
   static const String registerEndpoint = 'register';
