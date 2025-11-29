2aa0e06d6d2729abd594bd551d4c649f13aa596a
Toluwalase Bewaji <toluwalase.bewaji@yahoo.com>
2025-11-29 14:16:49 +0100

    feat: Added a bloclistener to listen if the user is login to not logout the user

--- lib/features/splash_screens/splash_screen_logo.dart
+++ lib/features/splash_screens/splash_screen_logo.dart
@@ -1,6 +1,10 @@
 import 'package:ecobin/core/presentation/constants/images.dart';
+import 'package:ecobin/features/auth/presentation/pages/signIn/sign_in.dart';
+import 'package:ecobin/features/auth/presentation/state/bloc/login_bloc.dart';
+import 'package:ecobin/features/home_page/presentation/pages/home_screen.dart';
 import 'package:ecobin/features/splash_screens/splash_screens.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 
 class SplashScreenLogo extends StatefulWidget {
@@ -22,15 +26,28 @@ class _SplashScreenLogoState extends State<SplashScreenLogo> {
         context.go(SplashScreens.routeName);
       }
     });
+
+    context.read<AuthBloc>().add(CheckAuthStatus());
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Image.asset(
-          AppImages.kLogo,
-          width: MediaQuery.of(context).size.width * 0.40,
+    return BlocListener<AuthBloc, AuthSate>(
+      listener: (context, state) {
+        if (state is AuthSuccess) {
+          // Navigate to home screen or dashboard
+          context.go(HomeScreen.routeName);
+        } else if (state is AuthInitial || state is AuthFailure) {
+          // Navigate to login screen
+          context.go(SignIn.routeName);
+        }
+      },
+      child: Scaffold(
+        body: Center(
+          child: Image.asset(
+            AppImages.kLogo,
+            width: MediaQuery.of(context).size.width * 0.40,
+          ),
         ),
       ),
     );
